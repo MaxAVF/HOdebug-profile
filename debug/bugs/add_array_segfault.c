a45e183d943c647c4b78b1789ae07c60100de5c9
MAXAVF <maxi_avf@hotmail.com>
2019-07-18 17:33:31 -0300

    folder 'bugs' done

--- debug/bugs/add_array_segfault.c
+++ debug/bugs/add_array_segfault.c
@@ -4,7 +4,7 @@
 int add_array(int *a, int *b, int n){
   int sum = 0;
   int i = 0;
-  for (i = 0; i <= n + 1; i++) {
+  for (i = 0; i < n ; i++) {
     sum += abs(a[i]);
     sum += abs(b[i]);
   };
@@ -12,9 +12,11 @@ int add_array(int *a, int *b, int n){
 }
 
 int main(int argc, char **argv) {
-  int *a=0, *b=0;
+  int *a, *b;
   int n = 3;
   int i, sum;
+  a = malloc(sizeof(int) * 3);
+  b = malloc(sizeof(int) * 3);
   for (i = 0; i < n; i++) {
     a[i] = i;
     b[i] = i;
---
+++ debug/bugs/respuestas.md
@@ -0,0 +1,11 @@
+
+Correr el programa con un debugger, sin agregar flags de debug. ¿Tienen toda la información que requerían?
+No
+
+¿Qué pasa si ponen el flag de debug? ¿Qué flag de optimización es el mejor para debuggear?
+-g
+
+Agreguen algún flag para que informe todos los warnings en la compilación, como -Wall. ¿Alguno les da alguna pista de por qué el programa se rompe?
+
+Si
+
